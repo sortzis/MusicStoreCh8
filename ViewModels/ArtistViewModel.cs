a38a824eece9d077483df8cc1744f822c7fb7bb8
sortzis <sortzis@mail.nmc.edu>
2022-12-06 18:52:58 -0500

    Add project files.

---
+++ ViewModels/ArtistViewModel.cs
@@ -0,0 +1,13 @@
+﻿using MusicStore.Models;
+using System.Collections.Generic;
+
+namespace MusicStore.ViewModels
+{
+    public class ArtistViewModel
+    {
+
+        public List<Album> Album { get; set; }
+        public List<Artist> Artist { get; set; }
+
+    }
+}
